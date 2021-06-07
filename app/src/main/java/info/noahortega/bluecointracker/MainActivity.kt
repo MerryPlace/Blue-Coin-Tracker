c5c1fc54dc0c64f067dfc9bf58c49426b115adf3
Noah Ortega <38738663+NoahOrtega@users.noreply.github.com>
2021-06-07 01:13:15 -0400

    Added dark mode menu item
    -Implemented basic dark mode (works on all versions! (I think))
    -Added a menu item that reflects current theme
    -On tap, the item cycles theme between "System default", "Light", and "Dark".

--- app/src/main/java/info/noahortega/bluecointracker/MainActivity.kt
+++ app/src/main/java/info/noahortega/bluecointracker/MainActivity.kt
@@ -5,24 +5,40 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myMenu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        if(AppCompatDelegate.getDefaultNightMode() == MODE_NIGHT_UNSPECIFIED) {
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_items, menu)
+        myMenu = menu!!
+        setThemeText();
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.credits_item -> creditsDialog()
-            else -> Toast.makeText(this, "Error: no item selected", Toast.LENGTH_SHORT).show()
+            R.id.themes_item -> themeTapped()
+            else -> Toast.makeText(this, "Error: invalid menu item", Toast.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
     }
@@ -30,5 +46,39 @@ class MainActivity : AppCompatActivity() {
     private fun creditsDialog() {
         val dialog = CreditsDialogue()
         dialog.show(supportFragmentManager, "wazoo")
+    }
+
+    private fun themeTapped() {
+        when(val theme = AppCompatDelegate.getDefaultNightMode()) {
+            MODE_NIGHT_FOLLOW_SYSTEM -> {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+            MODE_NIGHT_NO -> {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            }
+            MODE_NIGHT_YES -> {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            else -> {
+                Toast.makeText(this, "Error: invalid theme state ($theme)", Toast.LENGTH_SHORT).show()
+            }
+        }
+        setThemeText()
+    }
+
+    private fun setThemeText() {
+        val themeItem: MenuItem = myMenu.findItem(R.id.themes_item)
+
+        when(AppCompatDelegate.getDefaultNightMode()) {
+            MODE_NIGHT_FOLLOW_SYSTEM -> {
+                themeItem.title = "Theme: System Default"
+            }
+            MODE_NIGHT_NO -> {
+                themeItem.title = "Theme: Light"
+            }
+            MODE_NIGHT_YES -> {
+                themeItem.title = "Theme: Dark"
+            }
+        }
     }
 }
